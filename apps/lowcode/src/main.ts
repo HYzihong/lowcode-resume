e0e7ceb52861081138a475ef544f02e3c526dbb9
hy <zihong2020@foxmail.com>
2022-05-15 21:00:11 +0800

    chore(lowcode):update package.json,add @resume-cook/shared

--- apps/lowcode/src/main.ts
+++ apps/lowcode/src/main.ts
@@ -1,11 +1,25 @@
+/*
+ * @Author: hy
+ * @Date: 2022-05-13 21:59:02
+ * @LastEditors: hy
+ * @Description:
+ * @LastEditTime: 2022-05-15 20:30:04
+ * @FilePath: /resume-cook/apps/lowcode/src/main.ts
+ * Copyright 2022 hy, All Rights Reserved.
+ * 仅供学习使用~
+ */
 import { ViteSSG } from 'vite-ssg'
 import { setupLayouts } from 'virtual:generated-layouts'
+import sum from '@resume-cook/shared'
 import App from './App.vue'
 import generatedRoutes from '~pages'
 
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
+
+// eslint-disable-next-line no-console
+console.log(sum(1, 1))
 
 const routes = setupLayouts(generatedRoutes)
 
