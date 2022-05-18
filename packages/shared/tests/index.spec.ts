ae26ebcfc4b5e3320c543579d39084a48a266b9b
hy <zihong2020@foxmail.com>
2022-05-19 00:19:01 +0800

    feat(shared):use nanoid

--- packages/shared/tests/index.spec.ts
+++ packages/shared/tests/index.spec.ts
@@ -3,14 +3,16 @@
  * @Date: 2022-05-15 17:03:09
  * @LastEditors: hy
  * @Description: 
- * @LastEditTime: 2022-05-15 17:16:29
+ * @LastEditTime: 2022-05-18 23:28:39
  * @FilePath: /resume-cook/packages/shared/tests/index.spec.ts
  * Copyright 2022 hy, All Rights Reserved. 
  * 仅供学习使用~
  */
 import { describe, expect, test } from 'vitest';
-import { sum } from '../src'
 
+function sum(a: number, b: number): number {
+  return a + b
+}
 describe('shard:', () => {
   test('happy path', () => {
     expect(sum(1, 1)).toBe(2)
