1640c66a32bf9817cdc1a35299c985acfdb82f4e
hy <zihong2020@foxmail.com>
2022-05-25 23:23:20 +0800

    feat(lowcode): use loadCompScript load components script

--- apps/lowcode/src/main.ts
+++ apps/lowcode/src/main.ts
@@ -3,23 +3,54 @@
  * @Date: 2022-05-13 21:59:02
  * @LastEditors: hy
  * @Description:
- * @LastEditTime: 2022-05-15 20:30:04
+ * @LastEditTime: 2022-05-25 23:19:48
  * @FilePath: /resume-cook/apps/lowcode/src/main.ts
  * Copyright 2022 hy, All Rights Reserved.
  * 仅供学习使用~
  */
 import { ViteSSG } from 'vite-ssg'
 import { setupLayouts } from 'virtual:generated-layouts'
-import sum from '@resume-cook/shared'
+import type { RMaterial } from '@resume-cook/shared'
+import { Project } from '@resume-cook/shared'
 import App from './App.vue'
+import { loadCompScript } from './utlis'
 import generatedRoutes from '~pages'
 
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
 
+const materialList: RMaterial[] = [
+  {
+    id: '',
+    type: 'component',
+    category: {
+      name: '基础组件',
+    },
+    // 当前默认版本
+    version: '0.0.1',
+    source: 'rc-image.0.0.1.umd.js',
+    name: 'LcImage',
+    title: '图片',
+    // 预览图
+    thumbnail: '',
+    //
+    data: [
+      {
+        version: '0.0.1',
+        source: 'rc-image.0.0.1.umd.js',
+      },
+    ],
+  },
+]
+
 // eslint-disable-next-line no-console
-console.log(sum(1, 1))
+console.log(Project.create())
+
+// loadCompScript('rc-image.0.0.1.umd.js').then(() => {
+//   // eslint-disable-next-line no-console
+//   console.log((window as any).RCImage)
+// })
 
 const routes = setupLayouts(generatedRoutes)
 
@@ -29,6 +60,17 @@ export const createApp = ViteSSG(
   { routes, base: import.meta.env.BASE_URL },
   (ctx) => {
     // install all modules under `modules/`
+    // console.log('ctx ==>', ctx);
+    // ctx.app.component('rc-image', (window as any).RCImage.render)
+
+    // eslint-disable-next-line array-callback-return
+    Promise.all(materialList.map((m: RMaterial) => {
+      loadCompScript(m.source).then(() => {
+        const { render } = (window as any).RCImage
+        ctx.app.component('rc-image', render)
+      })
+    }))
+
     Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
   },
 )
