ef8053bba33f717f1812b9d1bccde6372b36126b
hy <zihong2020@foxmail.com>
2022-05-31 23:57:50 +0800

    fix(all):fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
+/**
+ * @Author: hy
+ * @Date: 2022-05-26 11:16:43
+ * @LastEditors: hy
+ * @Description:
+ * @LastEditTime: 2022-05-31 23:57:27
+ * @FilePath: /resume-cook/.eslintrc.js
+ * @Copyright 2022 hy, All Rights Reserved.
+ * @仅供学习使用~
+ **/
 const { defineConfig } = require("eslint-define-config");
 
 module.exports = defineConfig({
@@ -10,7 +20,7 @@ module.exports = defineConfig({
     es6: true,
   },
   rules: {
-    quotes: ["never"],
+    quotes: ["off"],
     "@typescript-eslint/quotes": ["never"],
   },
 });
